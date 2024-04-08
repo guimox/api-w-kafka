5755ef3c8cb19661eec7028762a25b653efc3dbf
gxlpes <guilhermxlopes@gmail.com>
2024-04-08 07:53:22 -0300

    feat: aggregating the values that enters the kafka stream PixAggregator

--- pix-stream-cons/src/main/java/com/alura/pix/streams/PixAggregator.java
+++ pix-stream-cons/src/main/java/com/alura/pix/streams/PixAggregator.java
@@ -4,23 +4,23 @@ import com.alura.pix.dto.PixDTO;
 import com.alura.pix.serdes.PixSerdes;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Printed;
-import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PixAggregator {
 
-    @Autowired
+    @Autowired()
     public void aggregator(StreamsBuilder streamsBuilder) {
-        KStream<String, PixDTO> messageStream = streamsBuilder.stream("pix-topic", Consumed.with(Serdes.String(), PixSerdes.serdes()))
+        KTable<String, Double> messageStream = streamsBuilder.stream("pix-topic", Consumed.with(Serdes.String(), PixSerdes.serdes()))
                 .peek((key, value) -> System.out.println("Pix recebido: " + value.getChaveOrigem()))
-                .filter((key, value) -> value.getValor() > 1000);
+                .groupBy((key, value) -> value.getChaveOrigem())
+                        .aggregate(() -> 0.0,
+                                (key, value, aggregate) -> (aggregate + value.getValor()),
+                                Materialized.with(Serdes.String(), Serdes.Double()));
 
-        messageStream.print(Printed.toSysOut());
-        messageStream.to("pix-topic-verificao-fraude", Produced.with(Serdes.String(), PixSerdes.serdes()));
+        messageStream.toStream().print(Printed.toSysOut());
+        messageStream.toStream().to("pix-topic-verificao-fraude", Produced.with(Serdes.String(), Serdes.Double()));
     }
 }
